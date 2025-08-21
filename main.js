b96acbdb9a2df4edcbf9e8ed9df324c8dd3e875c
colevr <100182500+colevrya@users.noreply.github.com>
2025-08-20 23:09:15 -0500

    jk;jkjo

--- main.js
+++
@@ -1,4 +0,0 @@
-import {DiscordSDK} from 'https://cdn.skypack.dev/@discord/embedded-app-sdk'
-const clientId = new URLSearchParams(window.location.search).get('client_id') || ''
-const sdk = new DiscordSDK(clientId)
-await sdk.ready()
